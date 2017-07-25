dfc2cd347af16d4dff8550e58569c4b2819e4efb
courier-new <kellirockwell@mail.com>
2017-07-24 20:13:36 -0700

    add startOfCUWiP to list of global variables

--- js/global.js
+++ js/global.js
@@ -5,6 +5,8 @@ let applyOpen = '1 September 2017 00:00:00 EDT';
 let applyClose = '14 October 2017 23:59:00 EDT';
 let registerOpen = '9 November 2017 00:00:00 PST';
 let registerClose = '18 November 2017 00:00:00 PST';
+// Set date/time of event in this format
+let startOfCUWiP = '12 January 2018 18:00:00 PDT';
 
 // Identify main navigation menu
 let $nav = $('nav.main.menu');
