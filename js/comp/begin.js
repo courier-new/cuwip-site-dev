15129ff678dd676fa6798a6ccb06554836a9d121
courier-new <kellirockwell@mail.com>
2017-09-11 20:48:18 -0700

    remove unnecessary legacy function

--- js/comp/begin.js
+++ js/comp/begin.js
@@ -1,12 +1,2 @@
 // Outer wrapper
 (function () {
-
-	$(document).on('ready', function(){
-		// Gather array of all pages
-		var pages = $('.page');
-
-		// Mark each section as ready
-		pages.each(function(){
-			$(this).addClass('ready')
-		});
-	});
